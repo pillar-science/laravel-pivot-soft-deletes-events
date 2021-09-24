f7c43d2225daa6a4b8484312c5e6a50d754acc63
Mathieu Tanguay <mathieu@pillar.science>
2021-09-23 22:27:25 -0400

    add proper relationship

--- src/Concerns/HasPivotSoftDeletesEventsRelationships.php
+++ src/Concerns/HasPivotSoftDeletesEventsRelationships.php
@@ -3,8 +3,17 @@
 namespace PillarScience\LaravelPivotSoftdeletesEvents\Concerns;
 
 use Fico7489\Laravel\Pivot\Traits\PivotEventTrait;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use PillarScience\LaravelPivotSoftdeletesEvents\Relations\BelongsToManySoftDeletesEvents;
 
 trait HasPivotSoftDeletesEventsRelationships
 {
     use PivotEventTrait;
+
+    protected function newBelongsToMany(Builder $query, Model $parent, $table, $foreignPivotKey, $relatedPivotKey,
+                                                $parentKey, $relatedKey, $relationName = null)
+    {
+        return new BelongsToManySoftDeletesEvents($query, $parent, $table, $foreignPivotKey, $relatedPivotKey, $parentKey, $relatedKey, $relationName);
+    }
 }
